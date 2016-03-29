db6e74b523d8ab53ca3c4447ace5eed707b62323
Baptiste Langlade <baptouuuu@gmail.com>
2016-03-29 08:46:37 +0200

    add adapter interface

---
+++ AdapterInterface.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Filesystem;
+
+/**
+ * Layer between value objects and concrete implementation
+ */
+interface AdpaterInterface
+{
+    /**
+     * Persist the file to concrete filesystem
+     *
+     * @param FileInterface $file
+     *
+     * @return self
+     */
+    public function persist(FileInterface $file): self;
+
+    /**
+     * Return the file with the given name
+     *
+     * @param string $file
+     *
+     * @throws FileNotFoundException
+     *
+     * @return FileInterface
+     */
+    public function get(string $file): FileInterface;
+
+    /**
+     * Check if the file exists
+     *
+     * @param string $file
+     *
+     * @return bool
+     */
+    public function has(string $file): bool;
+}
