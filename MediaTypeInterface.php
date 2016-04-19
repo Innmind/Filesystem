b47bb079f4e8a515f4a90165914a321d0654fc60
Baptiste Langlade <baptouuuu@gmail.com>
2016-04-19 19:06:19 +0200

    add media type interface

---
+++ MediaTypeInterface.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Filesystem;
+
+interface MediaTypeInterface
+{
+    public function topLevel(): string;
+    public function subType(): string;
+    public function suffix(): string;
+
+    /**
+     * @return MapInterface<string, ParameterInterface>
+     */
+    public function parameters(): MapInterface;
+    public function __toString(): string;
+}
