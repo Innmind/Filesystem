7790a00b2d7dd47ed6c85522ced151245ee0aaa8
Baptiste Langlade <baptouuuu@gmail.com>
2016-11-06 16:43:38 +0100

    add null stream

---
+++ src/Stream/NullStream.php
@@ -0,0 +1,84 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Filesystem\Stream;
+
+use Innmind\Filesystem\{
+    StreamInterface,
+    Exception\PositionNotSeekableException
+};
+
+final class NullStream implements StreamInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function __toString(): string
+    {
+        return '';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function close(): StreamInterface
+    {
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function size(): int
+    {
+        return 0;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function knowsSize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function position(): int
+    {
+        return 0;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isEof(): bool
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function seek(int $position, int $whence = self::SEEK_SET): StreamInterface
+    {
+        throw new PositionNotSeekableException;
+    }
+
+    /**
+     * {@inheritd oc}
+     */
+    public function rewind(): StreamInterface
+    {
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function read(int $length): string
+    {
+        return '';
+    }
+}
---
+++ tests/Stream/NullStreamTest.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\Filesystem\Stream;
+
+use Innmind\Filesystem\{
+    Stream\NullStream,
+    StreamInterface,
+    Exception\PositionNotSeekableException
+};
+
+class NullStreamTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInterface()
+    {
+        $stream = new NullStream;
+
+        $this->assertInstanceOf(StreamInterface::class, $stream);
+        $this->assertSame('', (string) $stream);
+        $this->assertSame($stream, $stream->close());
+        $this->assertTrue($stream->knowsSize());
+        $this->assertSame(0, $stream->size());
+        $this->assertSame(0, $stream->position());
+        $this->assertTrue($stream->isEof());
+        $this->assertSame($stream, $stream->rewind());
+        $this->assertSame('', $stream->read(42));
+        $this->expectException(PositionNotSeekableException::class);
+        $stream->seek(42);
+    }
+}
