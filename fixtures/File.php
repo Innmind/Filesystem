72aa505a9c7867a76db5b66b9acf855cd76a70ff
Baptiste Langlade <baptouuuu@gmail.com>
2020-05-02 17:18:50 +0200

    make stream seeking optional to simulate unopened files

--- fixtures/File.php
+++ fixtures/File.php
@@ -17,23 +17,29 @@ final class File
     {
         return Set\Composite::immutable(
             static function($name, $content, $mediaType, $seek): Model {
-                // as the generated seeked position may be higher than the actual
-                // content size
-                $seek = \min(\strlen($content), $seek);
-
                 $file = new Model(
                     $name,
                     $stream = Stream::ofContent($content),
                     $mediaType,
                 );
-                $stream->seek(new Position($seek));
+
+                if (\is_int($seek)) {
+                    // as the generated seeked position may be higher than the
+                    // actual content size
+                    $seek = \min(\strlen($content), $seek);
+
+                    $stream->seek(new Position($seek));
+                }
 
                 return $file;
             },
             Name::any(),
             Set\Strings::any(),
             MediaType::any(),
-            Set\Integers::between(0, 128), // 128 is the max string length by default
+            new Set\Either(
+                Set\Integers::between(0, 128), // 128 is the max string length by default
+                Set\Elements::of(null),
+            ),
         );
     }
 }
