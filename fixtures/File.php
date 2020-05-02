e0566a8ec95aa840a5afb0e3d9ccc4abb5ad6316
Baptiste Langlade <baptouuuu@gmail.com>
2020-05-02 17:01:33 +0200

    randomly seek stream to simulate user reading files

--- fixtures/File.php
+++ fixtures/File.php
@@ -5,7 +5,10 @@ namespace Fixtures\Innmind\Filesystem;
 
 use Innmind\Filesystem\File\File as Model;
 use Innmind\BlackBox\Set;
-use Innmind\Stream\Readable\Stream;
+use Innmind\Stream\{
+    Readable\Stream,
+    Stream\Position,
+};
 use Fixtures\Innmind\MediaType\MediaType;
 
 final class File
@@ -13,14 +16,24 @@ final class File
     public static function any(): Set
     {
         return Set\Composite::immutable(
-            static fn($name, $content, $mediaType): Model => new Model(
-                $name,
-                Stream::ofContent($content),
-                $mediaType,
-            ),
+            static function($name, $content, $mediaType, $seek): Model {
+                // as the generated seeked position may be higher than the actual
+                // content size
+                $seek = \min(\strlen($content), $seek);
+
+                $file = new Model(
+                    $name,
+                    $stream = Stream::ofContent($content),
+                    $mediaType,
+                );
+                $stream->seek(new Position($seek));
+
+                return $file;
+            },
             Name::any(),
             Set\Strings::any(),
             MediaType::any(),
+            Set\Integers::between(0, 128), // 128 is the max string length by default
         );
     }
 }
