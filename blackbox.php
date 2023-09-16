7b266f763b3000e24cd89a9972574607822aa682
Baptiste Langlade <baptouuuu@gmail.com>
2023-09-16 17:29:54 +0200

    simplify blackbox config

--- blackbox.php
+++ blackbox.php
@@ -13,18 +13,21 @@ use Innmind\BlackBox\{
 \ini_set('memory_limit', '-1');
 
 Application::new($argv)
-    ->codeCoverage(
-        CodeCoverage::of(
-            __DIR__.'/src/',
-            __DIR__.'/proofs/',
-            __DIR__.'/fixtures/',
-        )
-            ->dumpTo('coverage.clover')
-            ->enableWhen(\getenv('ENABLE_COVERAGE') !== false),
+    ->disableMemoryLimit()
+    ->scenariiPerProof(20)
+    ->when(
+        \getenv('ENABLE_COVERAGE') !== false,
+        static fn(Application $app) => $app
+            ->codeCoverage(
+                CodeCoverage::of(
+                    __DIR__.'/src/',
+                    __DIR__.'/proofs/',
+                    __DIR__.'/fixtures/',
+                )
+                    ->dumpTo('coverage.clover')
+                    ->enableWhen(true),
+            )
+            ->scenariiPerProof(1),
     )
-    ->scenariiPerProof(match (\getenv('ENABLE_COVERAGE')) {
-        false => 20,
-        default => 1,
-    })
     ->tryToProve(Load::everythingIn(__DIR__.'/proofs/'))
     ->exit();
